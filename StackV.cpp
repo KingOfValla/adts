d0f518271e7a1f9b54d23b0e7a7e4f948db16986
KingOfValla <KingofValla@users.noreply.github.com>
2018-03-21 13:14:19 -0400

    StackV.cpp

--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,36 @@
 #include "StackV.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+	int Stack::size()
+	{
+		return data.size();
+	}
+
+	void Stack::push(int value)
+	{
+		data.push_back(value);
+	}
+
+	void Stack::pop()
+	{
+		data.pop_back();
+	}
+
+	int Stack::top()
+	{
+		if(data.back() > -1 )
+			return data.back();
+		else
+			cout <<"The Stack is Empty" << endl;
+	}
+
+
+	void Stack::clear()
+	{
+		while(data.back() > -1)
+		{	data.pop_back();	}
+	}
+	
